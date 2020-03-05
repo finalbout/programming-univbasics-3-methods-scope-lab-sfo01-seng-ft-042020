e2f76aeab22ca642f652109b8ca6665d2320c573
John Tom Jr <johntjr.22@gmail.com>
2020-03-05 08:51:03 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,16 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  puts phrase = "It's-a me, Mario!"
 end
 
 def toadstool
-  puts status
+  puts status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+end
+
+def link 
+  puts cool = "It's Dangerous To Go Alone! Take This."
+end
+
+def  all_phrases do
+  expect{all_phrases}.to output #{mario}
 end
